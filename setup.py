81b3996cc7b1529487f3649a45d93c73e96632d6
Aidan Lister <aidan@aidanlister.com>
2013-05-26 19:44:28 -0600

    Bump the sorlery version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sorlery',
-    version='1.0',
+    version='1.1',
     description="",
     author="Aidan Lister",
     author_email='aidan@aidanlister.com',
