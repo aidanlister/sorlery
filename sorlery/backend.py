74cf29f478212aeded1932f94ecb3696a018dd28
Aidan Lister <aidan@aidanlister.com>
2013-07-04 20:02:41 -0500

    Instead of trying to serve a dummy image and suffering from broken images on the second load, we just serve the original image until the thumbnail has been rebuilt.

--- sorlery/backend.py
+++ sorlery/backend.py
@@ -1,6 +1,8 @@
 from sorl.thumbnail.conf import settings, defaults as default_settings
 from sorl.thumbnail.helpers import tokey, serialize
 from sorl.thumbnail.images import DummyImageFile, ImageFile
+from sorl.thumbnail.kvstores.base import add_prefix
+from sorl.thumbnail.helpers import serialize, deserialize
 from sorl.thumbnail import default
 from sorl.thumbnail.parsers import parse_geometry
 from sorl.thumbnail.base import ThumbnailBackend
@@ -14,16 +16,19 @@ class QueuedThumbnailBackend(ThumbnailBackend):
     """
     def get_thumbnail(self, file_, geometry_string, **options):
         source = ImageFile(file_)
+
         for key, value in self.default_options.iteritems():
             options.setdefault(key, value)
-        # For the future I think it is better to add options only if they
-        # differ from the default settings as below. This will ensure the same
-        # filenames beeing generated for new options at default.
+
         for key, attr in self.extra_options:
             value = getattr(settings, attr)
             if value != getattr(default_settings, attr):
                 options.setdefault(key, value)
+
+        # Generate a name for the thumbnail
         name = self._get_thumbnail_filename(source, geometry_string, options)
+
+        # See if we've got a hit in the cache
         thumbnail = ImageFile(name, default.storage)
         cached = default.kvstore.get(thumbnail)
         if cached:
@@ -37,27 +42,26 @@ class QueuedThumbnailBackend(ThumbnailBackend):
         # clear the corresponding row from the kvstore to have thumbnail rebuilt.
         job = create_thumbnail.delay(file_, geometry_string, options, name)
         if job:
-            # We don't want to do any file access in this thread, so we need
-            # to create dummy images for the thumbnail and the image that will
-            # be replaced in the queued job.
-            #
-            # I wasn't able to get this working with the key and serialisedstorage stuff,
-            # so for the moment it simply returns dummy images on the first request
-            # and a 404 on all future requests until the thumbnail is built.
-            temp_thumbnail = TemporaryDummyImageFile(geometry_string)
-            thumbnail.set_size(parse_geometry(geometry_string))
-
+            geometry = parse_geometry(geometry_string)
             # We can't add a source row to the kvstore without the size
             # information being looked up, so add dummy information here
             # We'll need to correct this information when we generate the thumbnail
-            source.set_size(parse_geometry(geometry_string))
-
+            source.set_size(geometry)
             default.kvstore.get_or_set(source)
+
+            # We don't want to do any file access in this thread, so we tell sorlery
+            # to proceed as normal and cheekily update the name and storage after
+            # the hash has been calculated.
+            thumbnail.set_size(geometry)
             default.kvstore.set(thumbnail, source)
 
-        return temp_thumbnail
+            # Now we go back and manually update the thumbnail to point at the source image
+            # Hopefully someone can suggest a better way to do this ... but the sorl internals
+            # don't make it easy to.
+            rawvalue = default.kvstore._get_raw(add_prefix(thumbnail.key))
+            rawvaluearr = deserialize(rawvalue)
+            rawvaluearr['name'] = file_.name
+            default.kvstore._set_raw(add_prefix(thumbnail.key), serialize(rawvaluearr))
 
-
-class TemporaryDummyImageFile(DummyImageFile):
-    pass
-
+        thumbnail.name = file_.name
+        return thumbnail
